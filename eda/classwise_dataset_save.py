d7140def5b5a858a697cbbb6ef7ad1c26e8b08a4
zionia4758 <zionia4758@gmail.com>
2023-05-17 07:34:33 +0000

    class분류기 수정

--- eda/classwise_dataset_save.py
+++ eda/classwise_dataset_save.py
@@ -3,7 +3,7 @@ from eda_fns import *
 import eda_fns
 import matplotlib.pyplot as plt
 
-target = 'val'
+target = 'train'
 
 data_df, ann_df = json_to_df(f'../../dataset/{target}.json')
 with open(f'../../dataset/{target}.json','r') as f:
@@ -12,8 +12,8 @@ with open(f'../../dataset/{target}.json','r') as f:
 for idx in range(10):
     split_ann_df = ann_df[ann_df['category_id']==idx]    
     split_data_df = data_df[data_df['id'].isin(split_ann_df['image_id'].unique())]
-    save_json['images'] = [split_data_df.to_dict('records')]
-    save_json['annotations'] = [split_ann_df.to_dict('records')]
+    save_json['images'] = split_data_df.to_dict('records')
+    save_json['annotations'] = split_ann_df.to_dict('records')
     print(f'idx:{idx}, split_data:{len(split_data_df)},split_ann:{len(split_ann_df)}')
     with open(f'./classwise_dataset/{target}_{idx}.json','w') as f:
         json.dump(save_json,f,indent=2)
