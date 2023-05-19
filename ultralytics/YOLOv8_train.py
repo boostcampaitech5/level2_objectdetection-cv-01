b0060969ea0c44eac04a317e4de80183e8426184
Hyeji0828 <janghyeji0828@gmail.com>
2023-05-19 02:44:11 +0000

    add hyperparameter options

--- ultralytics/YOLOv8_train.py
+++ ultralytics/YOLOv8_train.py
@@ -3,11 +3,20 @@ from wandb.integration.yolov8 import add_callbacks
 
 if __name__ == '__main__':
     # Load pretrained model
-    model = YOLO("yolov8n.pt")
-    # model = YOLO("best.pt")
+    # model = YOLO("yolov8n.pt")
+    model = YOLO("yolov8x.pt")
+
+    # hyperparameter
+    img_size = 640
+    epoch = 100
+    optimizer = 'RMSProp' # ['SGD', 'Adam', 'AdamW', 'RMSProp']
+    cos_lr = False # Cosine learning rate scheduler
+    label_smoothing = 0.0 # 0.1
 
     # Wandb
     add_callbacks(model, project="level2) Object Detection")
 
-    model.train(data="coco_recycle.yaml", epochs=500, batch=16, imgsz=512, save=True)
-    
+    model.train(data="coco_recycle.yaml", epochs=epoch, batch=16, imgsz=img_size, 
+                save=True, pretrained=True, val=True, save_json=True,
+                optimizer= optimizer, cos_lr = cos_lr, label_smoothing= label_smoothing)
+    model.val()
